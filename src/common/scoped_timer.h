04ad8de8b35a6712c50f9fd028f20b53f9f56eab
Surya Sathujoda <suryasathujoda@gmail.com>
2025-04-08 08:21:48 +0100

    feat: implemented scoped timer.

---
+++ src/common/scoped_timer.h
@@ -0,0 +1,23 @@
+#ifndef SCOPED_TIMER_H
+#define SCOPED_TIMER_H
+
+#include <chrono>
+#include <iostream>
+
+class ScopedTimer {
+public:
+	ScopedTimer(const char* label)
+		: label_{label},
+		  start_{std::chrono::high_resolution_clock::now()}{}
+
+	~ScopedTimer() {
+		auto end = std::chrono::high_resolution_clock::now();
+		auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start_);
+		std::cout << label_ << " took " << elapsed.count() << "ns" << std::endl;
+	}
+private:
+	const char* label_;
+	std::chrono::high_resolution_clock::time_point start_;
+};
+
+#endif
